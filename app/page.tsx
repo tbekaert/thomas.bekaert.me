fe814c99cf9210160b085cf4451f71b54c44452d
tbekaert <thomas@bekaert.me>
2025-09-23 11:56:00 +0200

    feat: Add prettier and switch to Inter font

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Image from 'next/image'
+
+import styles from './page.module.css'
 
 export default function Home() {
   return (
@@ -7,39 +8,46 @@ export default function Home() {
       <main className={styles.main}>
         <Image
           className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
+          src='/next.svg'
+          alt='Next.js logo'
           width={180}
           height={38}
           priority
         />
-        <ol>
+        {/* <ol>
+          <li>{t('metaTitle')}</li>
+          <li>{locale}</li>
           <li>
-            Get started by editing <code>app/page.tsx</code>.
+            <Link locale='fr' href='/'>
+              FR
+            </Link>{' '}
+            |{' '}
+            <Link locale='en' href='/'>
+              EN
+            </Link>
           </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        </ol> */}
 
         <div className={styles.ctas}>
           <a
             className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
           >
             <Image
               className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
+              src='/vercel.svg'
+              alt='Vercel logomark'
               width={20}
               height={20}
             />
             Deploy now
           </a>
           <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+            target='_blank'
+            rel='noopener noreferrer'
             className={styles.secondary}
           >
             Read our docs
@@ -48,42 +56,42 @@ export default function Home() {
       </main>
       <footer className={styles.footer}>
         <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/file.svg"
-            alt="File icon"
+            src='/file.svg'
+            alt='File icon'
             width={16}
             height={16}
           />
           Learn
         </a>
         <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/window.svg"
-            alt="Window icon"
+            src='/window.svg'
+            alt='Window icon'
             width={16}
             height={16}
           />
           Examples
         </a>
         <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           <Image
             aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
+            src='/globe.svg'
+            alt='Globe icon'
             width={16}
             height={16}
           />
@@ -91,5 +99,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  );
+  )
 }
