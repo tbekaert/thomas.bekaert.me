3dbdebe9a4589ee903618778160ff62bcecddb20
tbekaert <thomas@bekaert.me>
2025-09-25 12:22:57 +0200

    fix: Move skip to inside header for accessibility

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,14 @@
 import { getData } from '@/data'
 
 import styles from './Header.module.css'
+import { SkipTo } from './SkipTo'
 
 export const Header = async () => {
   const data = await getData('en')
   return (
     <>
       <header className={styles.header}>
+        <SkipTo />
         <h1 className={styles.title}>{data.header.title}</h1>
         <h2 className={styles.subtitle}>{data.header.subtitle}</h2>
       </header>
